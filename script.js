b2d8c228633231c54419acdc32808f956a6d49e4
uniqcle <uniqcle@yandex.ru>
2021-04-02 10:52:14 +0700

    calculator completed

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ class Calculator{
 
     chooseOperation(operation){
         if( this.curOperand === '') return;
-        if( this.prevOperand !== '') this.compute();
+        if( this.prevOperand !== '') {
+            this.compute();
+        }
         this.operation = operation;
         this.prevOperand = this.curOperand;
         this.curOperand = '';
@@ -29,38 +31,62 @@ class Calculator{
     }
 
     compute(){
-        let computation;
-        let prev = parseFloat( this.prevOperand );
-        let cur = parseFloat( this.curOperand );
-        switch( this.operation ){
+        let computation
+        const prev = parseFloat(this.prevOperand)
+        const current = parseFloat(this.curOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
             case '+':
-                computation = prev + cur;
-                break;
+                computation = prev + current
+                break
             case '-':
-                computation = prev - cur;
-                break;
-            case '%':
-                computation = prev / cur;
-                break;
+                computation = prev - current
+                break
             case '*':
-                computation = prev * cur;
-                break;
-            default: return;
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
         }
-
-        this.curOperand = computation;
-        this.operation = undefined;
-        this.prevOperand = '';
+        this.curOperand = computation
+        this.operation = undefined
+        this.prevOperand = ''
 
     }
 
-    updateDisplay(){
-        this.curOperandElement.innerText = this.curOperand;
-        this.prevOperandElement.innerText = this.prevOperand;
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
     }
 
+    updateDisplay() {
+        this.curOperandElement.innerText =
+            this.getDisplayNumber(this.curOperand)
+        if (this.operation != null) {
+            this.prevOperandElement.innerText =
+                `${this.getDisplayNumber(this.prevOperand)} ${this.operation}`
+        } else {
+            this.prevOperandElement.innerText = ''
+        }
+    }
 
 }
+
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
@@ -80,19 +106,26 @@ numberButtons.forEach(button => {
 });
 
 operationButtons.forEach( button => {
-    button.addEventListener('click', button=> {
+    button.addEventListener('click', ()=> {
         calculator.chooseOperation( button.innerText );
-
+        calculator.updateDisplay()
     })
 })
 
-allClearButton.addEventListener('click', ()=>{
+allClearButton.addEventListener('click', (button)=>{
     calculator.clear();
     calculator.updateDisplay();
 });
 
-deleteButton.addEventListener('click', () => {
+deleteButton.addEventListener('click', (button) => {
     calculator.delete();
     calculator.updateDisplay();
 })
+
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
 
